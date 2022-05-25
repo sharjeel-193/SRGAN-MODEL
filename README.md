ac9214b93849c5f38bb9925f7377138848447eb2
sharjeel-193 <msmaqsood.bscs19seecs@seecs.edu.pk>
2022-05-25 17:40:47 +0500

    Evaluation

---
+++ README.md
@@ -0,0 +1,69 @@
+# SRGAN
+A PyTorch implementation of SRGAN based on CVPR 2017 paper 
+[Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network](https://arxiv.org/abs/1609.04802).
+
+## Requirements
+- [Anaconda](https://www.anaconda.com/download/)
+- PyTorch
+```
+conda install pytorch torchvision -c pytorch
+```
+- opencv
+```
+conda install opencv
+```
+
+## Datasets
+
+DIV 2k
+https://data.vision.ee.ethz.ch/cvl/DIV2K/
+
+## Usage
+
+### Train
+```
+python train.py
+
+optional arguments:
+--crop_size                   training images crop size [default value is 88]
+--upscale_factor              super resolution upscale factor [default value is 4](choices:[2, 4, 8])
+--num_epochs                  train epoch number [default value is 100]
+```
+The output val super resolution images are on `training_results` directory.
+
+### Test Benchmark Datasets
+```
+python test_benchmark.py
+
+optional arguments:
+--upscale_factor              super resolution upscale factor [default value is 4]
+--model_name                  generator model epoch name [default value is netG_epoch_4_100.pth]
+```
+The output super resolution images are on `benchmark_results` directory.
+
+### Test Single Image
+```
+python test_image.py
+
+optional arguments:
+--upscale_factor              super resolution upscale factor [default value is 4]
+--test_mode                   using GPU or CPU [default value is 'GPU'](choices:['GPU', 'CPU'])
+--image_name                  test low resolution image name
+--model_name                  generator model epoch name [default value is netG_epoch_4_100.pth]
+```
+The output super resolution image are on the same directory.
+
+### Test Single Video
+```
+python test_video.py
+
+optional arguments:
+--upscale_factor              super resolution upscale factor [default value is 4]
+--video_name                  test low resolution video name
+--model_name                  generator model epoch name [default value is netG_epoch_4_100.pth]
+```
+The output super resolution video and compared video are on the same directory.
+
+## Benchmarks
+**Upscale Factor = 2**
+
---
+++ evaluate_images.py
@@ -0,0 +1,66 @@
+import argparse
+import time
+
+import torch
+from PIL import Image
+from torch.autograd import Variable
+from torchvision.transforms import ToTensor, ToPILImage
+import numpy as np
+import cv2
+
+from model import Generator
+
+import os
+
+def check_similarity(hrImg, outImg):
+  img1 = np.asarray(hrImg)
+  img2 = np.asarray(outImg)
+  errorL2 = cv2.norm( img1, img2, cv2.NORM_L2 )
+  percentage = 1 - errorL2 / ( img1.shape[0] * img1.shape[1] )
+  return percentage
+
+
+parser = argparse.ArgumentParser(description='Test Single Image')
+parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
+parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
+parser.add_argument('--image_name', type=str, help='test low resolution image name')
+parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+opt = parser.parse_args()
+
+UPSCALE_FACTOR = opt.upscale_factor
+TEST_MODE = True if opt.test_mode == 'GPU' else False
+IMAGE_NAME = opt.image_name
+MODEL_NAME = opt.model_name
+
+model = Generator(UPSCALE_FACTOR).eval()
+if TEST_MODE:
+    model.cuda()
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+else:
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+
+input_path = 'test_imgs/input'
+HR_path = 'test_imgs/hr'
+input_imgs = os.listdir(input_path)
+HR_imgs = os.listdir(HR_path)
+for file in input_imgs:
+  img_path = (os.path.join(input_path, file))
+  image = Image.open(img_path)
+  hr_img = Image.open(os.path.join(HR_path, file[0:4]+'.png'))
+  image = Variable(ToTensor()(image)).unsqueeze(0)
+  if TEST_MODE:
+      image = image.cuda()
+
+  start = time.process_time()
+  out = model(image)
+  elapsed = (time.process_time() - start)
+  print('For Image '+str(file))
+  print('\tcost ' + str(elapsed) + 's')
+  out_img = ToPILImage()(out[0].data.cpu())
+  print('\tshape ' + str(np.asarray(out_img).shape))
+  print('\tshape ' + str(np.asarray(hr_img).shape))
+  # score = ssim(np.asarray(out_img), np.asarray(hr_img))
+  print('Difference: '+ str(check_similarity(hr_img, out_img)))
+  out_img.save('test_imgs/output/SR_' + file)
+  # print(filename)
+
